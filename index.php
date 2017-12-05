7886c7f848ad9c192e7f47d1985a631acc41da99
Remdran <karlos287@googlemail.com>
2017-12-05 11:56:19 +0000

    Setting up initial file structure

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-<?php
+<?php include("includes/header.php"); ?>
+
+
+<div class="row">
+
+    <!-- Blog Entries Column -->
+    <div class="col-md-8">
+
+
+    </div>
+
+
+    <!-- Blog Sidebar Widgets Column -->
+    <div class="col-md-4">
+
+
+        <?php include("includes/sidebar.php"); ?>
+
+
+    </div>
+    <!-- /.row -->
+
+    <?php include("includes/footer.php"); ?>
